263de43bee74f05416e5f6a71765b99d402b3569
Marieke Bednarczyk <mbed@live.nl>
2017-01-03 11:11:06 +0100

    created the subscriber model

--- crmapp/urls.py
+++ crmapp/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from marketing.views import HomePage
 
+admin.autodiscover()
+
 urlpatterns = patterns('',
     # Marketing pages
     url(r'^$', HomePage.as_view(), name="home"),
