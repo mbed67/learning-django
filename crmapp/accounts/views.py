9cf01d62af79270d2844ee54381aabb4b9ff8480
Marieke Bednarczyk <mbed@live.nl>
2017-01-29 16:09:08 +0100

    added new account view

--- crmapp/accounts/views.py
+++ crmapp/accounts/views.py
@@ -1,9 +1,12 @@
-from django.views.generic import ListView
 from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
+from django.core.urlresolvers import reverse
 from django.http import HttpResponseForbidden
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.generic import ListView
 
+from .forms import AccountForm
 from .models import Account
 
 class AccountList(ListView):
@@ -42,3 +45,24 @@ def account_detail(request, uuid):
     }
 
     return render(request, 'accounts/account_detail.html', variables)
+
+@login_required()
+def account_cru(request):
+    form = AccountForm(request.POST)
+    if form.is_valid():
+        account = form.save(commit=False)
+        account.owner = request.user
+        account.save()
+        redirect_url = reverse(
+            'crmapp.accounts.views.account_detail',
+            args=(account.uuid,)
+        )
+        return HttpResponseRedirect(redirect_url)
+    else:
+        form = AccountForm()
+
+    variables = { 'form': form, }
+
+    template = 'accounts/account_cru.html'
+
+    return render(request, template, variables)
